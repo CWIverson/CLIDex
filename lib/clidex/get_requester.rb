e4a3d2ea7ebce62ae90755b16e8e0a897bac4d5f
christian iverson <chris.w.iverson@gmail.com>
2021-01-06 14:59:15 -0800

    built the get requester and json parser

--- lib/clidex/get_requester.rb
+++ lib/clidex/get_requester.rb
@@ -1,10 +1,30 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
 class GetRequester
     @@search_history=[]
+    attr_reader :category, :searched_item, :url
+    def initialize(search_category, search_term)
+        @category=search_category
+        @searched_item = search_term
+        @url= "https://pokeapi.co/api/v2/#{search_category}/#{search_term}"
 
-    def initialize(search_category, search term)
-
-
-
+    end
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+        #binding.pry
+    end
+    def json_parser
+        input = self.get_response_body
+        JSON.parse(input)
     end
 
 end
+
+object = GetRequester.new("pokemon-species", "charmander")
+
+puts object.json_parser
